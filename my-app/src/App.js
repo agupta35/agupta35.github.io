33d3cbae4c3c3a8d5b89c6b1ad0512aeae3716ac
akshit792 <akshit792@gmail.com>
2020-01-21 10:25:40 -0500

    Grid Layout and Nav Link bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+import Projects from './Components/Projects'; 
+import About from './Components/About'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container,Nav,Row} from 'react-bootstrap';
+import Col from 'react-bootstrap/Col'
 import './App.css';
 
-function App() {
+class App extends Component{
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid={true}>
+  <Row noGutters={true}>
+  <Col xs={3}>
+    <Row noGutters={true}>
+    Talk something about me
+    </Row>  
+    <Row noGutters={true}> 
+      <Nav defaultActiveKey="/home" className="flex-column">
+      <Nav.Link href="/home">Home</Nav.Link>
+      <Nav.Link eventKey="link-1">About</Nav.Link>
+      <Nav.Link eventKey="link-2">Projects</Nav.Link>
+      <Nav.Link eventKey="link-3">Resume</Nav.Link>
+      </Nav>
+    </Row>
+  </Col>
+
+  <Col>
+  <h1>Two</h1>
+  </Col>
+  </Row>
+</Container>
   );
 }
-
+}
 export default App;
